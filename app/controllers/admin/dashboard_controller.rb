a33d56eaee652d0bb4d26e680ff622f3a300b672
AndrewZapotochnyi <info@azblockchain.co>
2020-05-28 00:41:40 +0000

    Admin dashboard counts

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,6 +2,10 @@ class Admin::DashboardController < ApplicationController
 
   http_basic_authenticate_with name: ENV["HTTP_ADMIN_USER"], password: ENV["HTTP_ADMIN_PASSWORD"]
 
+
+
   def show
+    @products_count = Product.count(:all)
+    @categories_count = Category.count(:all)
   end
 end
